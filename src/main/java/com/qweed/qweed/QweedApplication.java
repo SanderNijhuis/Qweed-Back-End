928ab94a05a1754126a13e753c9d9380ede5af10
Sander <sander_nijhuis@live.nl>
2021-01-12 13:39:58 +0100

    intializing project

---
+++ src/main/java/com/qweed/qweed/QweedApplication.java
@@ -0,0 +1,13 @@
+package com.qweed.qweed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QweedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QweedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qweed/qweed/QweedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qweed.qweed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QweedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
